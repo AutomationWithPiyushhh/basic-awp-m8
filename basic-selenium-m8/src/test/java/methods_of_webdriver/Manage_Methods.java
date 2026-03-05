dc4d0dd6a8e0edde7bf99981f54e1c3f995006b5
AutomationWithPiyushhh <vasudevahir21@gmail.com>
2026-03-05 12:55:20 +0530

    Window

--- basic-selenium-m8/src/test/java/methods_of_webdriver/Manage_Methods.java
+++ basic-selenium-m8/src/test/java/methods_of_webdriver/Manage_Methods.java
@@ -1,5 +1,61 @@
 package methods_of_webdriver;
 
-public class Manage_Methods {
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class Manage_Methods {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+//		driver.manage().window().maximize();
+
+		driver.get("https://www.facebook.com/");
+
+//		size
+		Dimension dim = driver.manage().window().getSize();
+		System.out.println(dim);
+		
+		int w = dim.getWidth();
+		int h = dim.getHeight();
+		System.out.println(w + " and " + h);
+//
+//		Options opt = driver.manage();
+//		Window win = opt.window();
+//
+//		Thread.sleep(2000);
+//
+//		win.minimize();
+//		Thread.sleep(2000);
+//
+//		System.out.println(win.getSize());
+//
+//		win.fullscreen();
+//		System.out.println(win.getSize());
+
+//		position/location
+
+		Point pt = driver.manage().window().getPosition();
+		System.out.println(pt);
+
+		int X = pt.getX();
+		int Y = pt.getY();
+		
+		System.out.println(X + " and " + Y);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		Thread.sleep(3000);
+		driver.quit();
+	}
 }
